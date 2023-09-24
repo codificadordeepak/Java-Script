b3e1af8e8a6b7e9ecf23a11f5a2816076e4853d2
codificadordeepak <145889577+codificadordeepak@users.noreply.github.com>
2023-09-24 06:38:43 +0000

    Done

---
+++ Basics/01.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+// Code readability is most imp Never write like console,log(3+
+// tc39 and mdn reference are two main resources
+//Undefined value
+
+/*
+Different datatypes
+1.numbers upto 2 raise to power 53
+2. bigInt used for large intezers
+3. boolean => true/false
+4. String => "Name"
+5. Null => standalone value
+6. Undefined=> which is not defined till now
+7. Symbols => Unique
+8. Objects
+*/
+let age = 23
+let name = "Deepak"
+const account = 12345
+let IsloggedIn= true
+let city;
+
+console.log(age,"name",account,IsloggedIn,city);
+
